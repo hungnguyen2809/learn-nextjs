bf35c5390295bbba6748a532c6bb0ae1730634bf
Hung Nguyen <hungnguyen99.nvh@gmail.com>
2023-05-04 15:13:12 +0700

    cache getServerSideProps

--- src/pages/posts/params.tsx
+++ src/pages/posts/params.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React from 'react';
@@ -19,3 +20,23 @@ const OtherPostPage: React.FC = () => {
 };
 
 export default OtherPostPage;
+
+//mỗi lần request vào là chạy getServerSideProps
+export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+  //cache lại trang s-maxage <= 5s thì trả lại dữ liệu cũ, s-maxage > 5s gọi lại getServerSideProps trả data mới
+  context.res.setHeader('Cache-Control', 's-maxage=5');
+
+  //cache lại trang s-maxage<=5s thì trả lại dữ liệu cũ, s-maxage>5s thì trả lại data cũ đồng thời gọi lại 
+  // getServerSideProps => lần refresh tiếp theo mới có data mới
+  context.res.setHeader('Cache-Control', 's-maxage=5, stale-while-revalidate');
+
+  //cache lại trang s-maxage<=5s thì trả lại dữ liệu cũ, s-maxage>5s thì trả lại data cũ nếu stale-while-revalidate<=5, 
+  //khi stale-while-revalidate>5s gọi lại getServerSideProps => lần refresh tiếp theo mới có data mới
+  context.res.setHeader('Cache-Control', 's-maxage=5, stale-while-revalidate=5');
+
+  await new Promise((resolve) => setTimeout(resolve, 3000)); //fake shalow
+
+  return {
+    props: {},
+  };
+};
